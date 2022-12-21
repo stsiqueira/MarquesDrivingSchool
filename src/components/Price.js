270265f4823a1d606182a17dadbcbe57993e0ac6
Thiago <stsiqueira@outlook.com>
2022-12-20 20:29:57 -0800

    update price.
    update price.

--- src/components/Price.js
+++ src/components/Price.js
@@ -6,12 +6,12 @@ const Price = () => {
     <div className='PriceContainer'>
       <h2> Price</h2>
       <ul>
-        <li>✅ 1 lesson $95</li>
-        <li>✅ 2 lessons $170</li>
-        <li>✅ 3 lesson $235</li>
-        <li>✅ From fourth lessons  $70 per lesson</li>
-        <li>🚗 Car for the road test $70</li>
-        <li>⏱ 1:30 h classes</li>
+        <li>✅ 1 class $75 ⏰1:00h</li>
+        <li>✅ 1 class $95 ⏰1:30h</li>
+        <li>✅ 2 lessons $180 ⏰1:30h</li>
+        <li>✅ 3 lessons $260 ⏰1::30h</li>
+        <li>✅ $140 class ⏰2h</li>
+        <li>🚗$120 car for the road test + 30 minutes class before the road test.</li>
       </ul>
 
     </div>
