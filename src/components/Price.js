1678032b62174cb106730622b91f064d7110086c
Thiago Silva Siqueira <69597860+stsiqueira@users.noreply.github.com>
2025-09-25 10:10:40 -0700

    Update Price.js
    atulizando preco

--- src/components/Price.js
+++ src/components/Price.js
@@ -6,10 +6,16 @@ const Price = () => {
     <div className='PriceContainer'>
       <h2> Price</h2>
       <ul>
+        <li>Class 5</li>
         <li>✅ class $75 ⏰1:00h</li>
         <li>✅ class $110 ⏰1:30h</li>
         <li>✅ class $140 ⏰2:00h</li>
-        <li>✅ car rental for the road test $100</li>
+        <li></li>
+        <li>Class 7</li>
+        <li>✅ class $85 ⏰1:00h</li>
+        <li>✅ class $125 ⏰1:30h</li>
+        <li></li>
+        <li>✅ car rental for the road test $120</li>
       </ul>
     </div>
   )
